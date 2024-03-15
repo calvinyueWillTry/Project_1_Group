e9f8c64719f3b8a73c50944e4dea1be5b8b76493
jakepears <jake.pearson.1694@gmail.com>
2024-03-14 20:11:13 -0700

    less css added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 /** @format */
 
-// this is our injection file
+const sass = require('sass');
+
+const result = sass.compile('style.scss');
+console.log(result.css);
+
+const compressed = sass.compile('style.scss', { style: 'compressed' });
+console.log(compressed.css);
